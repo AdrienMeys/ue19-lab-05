d9976e168ac94d6b40dd2deda99843960f5d30a0
AdrienMeys <a.meys@student.helmo.be>
2025-11-14 08:34:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,7 @@
 import requests
-import sys
 
 def get_joke():
-    url = "https://official-joke-api.appspot.com/random_joke"
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        joke = response.json()
-        return f"{joke['setup']}\n{joke['punchline']}"
-    except requests.exceptions.RequestException as e:
-        return f"Erreur : {e}"
+    j = requests.get("https://official-joke-api.appspot.com/random_joke").json()
+    return f"{j['setup']}\n{j['punchline']}"
 
-if __name__ == "__main__":
-    print("Voici une blague :")
-    print(get_joke())
+print(get_joke())
